892d5d9b09ff4d74cd8ae61be6d9a6efa62f8c35
Regina Ruiz <regina.ruiz2004@gmail.com>
2026-08-30 12:23:29 -0400

    deleted how I build and How I research sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/Hero";
 import FocusStrip from "@/components/FocusStrip";
 import SelectedWork from "@/components/SelectedWork";
-import ProcessSection from "@/components/ProcessSection";
 import Skills from "@/components/Skills";
 import AboutTeaser from "@/components/AboutTeaser";
 import ContactCTA from "@/components/ContactCTA";
@@ -12,7 +11,6 @@ export default function Home() {
       <Hero />
       <FocusStrip />
       <SelectedWork />
-      <ProcessSection />
       <Skills />
       <AboutTeaser />
       <ContactCTA />
